1bc62b763f2185015e15e6e14b673098174fe039
JacobToresson <>
2019-08-22 11:19:30 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Level1.java
+++ src/main/java/Level1.java
@@ -5,7 +5,7 @@ public class Level1 {
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello I'm Duke");
         System.out.println("\tWhat can I do for you?");
-    String reply = "start";
+    String reply = "";
     Scanner input = new Scanner(System.in);
         while(!reply.equals("bye")) {
         System.out.println("\t____________________________________________________________\n");
@@ -17,6 +17,7 @@ public class Level1 {
     }
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________\n");
+
 }
 }
 
