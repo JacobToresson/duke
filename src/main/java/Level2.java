5df423ef5e8d7c31a46dd9e22d32a98fdb873ff8
JacobToresson <>
2019-08-22 11:37:40 +0800

    Level 2. Add, List

--- src/main/java/Level2.java
+++ src/main/java/Level2.java
@@ -1,2 +1,26 @@
+import java.util.Scanner; // https://www.programiz.com/java-programming/basic-input-output
+
 public class Level2 {
+    public static void main(String[] args){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        String reply = "";
+        Scanner input = new Scanner(System.in);
+        while(!reply.equals("bye")) {
+            System.out.println("\t____________________________________________________________\n");
+            reply = input.next();
+            System.out.println("\t____________________________________________________________");
+            if(reply.equals("bye")) {
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t____________________________________________________________\n");
+            }
+            else if(reply.equals("list")){
+                System.out.println("LIIISTANNNNN");
+            }
+            else{
+                System.out.println("\tadded: " + reply);
+            }
+        }
+    }
 }
