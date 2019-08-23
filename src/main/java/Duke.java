c8ca7e543d78f39b8e18eeb83307ae8cb2af9b73
JacobToresson <>
2019-08-23 19:48:45 +0800

    moving code from level1 file to duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,7 +23,6 @@ public class Duke {
         }
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________\n");
-
     }
 }
 
