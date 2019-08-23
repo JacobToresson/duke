2ea302e21254e99b8a1fca748164195eac503055
JacobToresson <>
2019-08-23 19:43:16 +0800

    moving changes from level 1 file to duke file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        String reply = "";
+        Scanner input = new Scanner(System.in);
+        while(!reply.equals("bye")) {
+            System.out.println("\t____________________________________________________________\n");
+            reply = input.next();
+            System.out.println("\t____________________________________________________________");
+            if(!reply.equals("bye")){
+                System.out.println("\t" + reply);
+            }
+        }
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________\n");
 
     }
 }
+
+
+
